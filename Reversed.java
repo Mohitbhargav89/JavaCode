741e91a37de173bd0f37c4fff660005f6c4276da
Mohitbhargav89 <141838133+Mohitbhargav89@users.noreply.github.com>
2023-09-25 23:31:34 +0530

    Add files via upload

--- Reversed.java
+++ Reversed.java
@@ -1,12 +1,17 @@
-public class Reversed {
-    public static void main(String[] args) {
-        String str1 =  ("mohitbhargav");
-        String str2 =   ("glauniversity");
-
-        String result1 = str1.toUpperCase();
-        String result2 = str2.toUpperCase();
-        System.out.println("str lower case :"+result1);
-        System.out.println("str lower case:"+result2);
+    import java.util.*;
+    public class Reversed {
+        public static void main(String args[]) {
+            try (//Print reverse
+            Scanner sc = new Scanner(System.in)) {
+                int n = sc.nextInt();//TAKING INPUT
+                int rev =  0;
+                while(n != 0) {
+                    int digit = n % 10;//last digit 
+                    rev = rev * 10 + digit;
+                    n /= 10;//Decreasing no.
+                }
+                System.out.println(rev);
+            }
+        }
+       
     }
-    
-}
